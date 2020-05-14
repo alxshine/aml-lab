47fb59fd99dcea4fc81bbd846b5035248a96ed92
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2020-05-14 15:44:49 +0200

    Clean up data for TensorFlow

--- data.py
+++ data.py
@@ -18,11 +18,18 @@ def load_mnist():
     
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     # normalize to [0, 1] and reshape to (-1, 28, 28, 1)
+    x_train = (x_train/255).reshape((-1,28,28,1))
+    x_test = (x_test/255).reshape((-1,28,28,1))
 
     # convert labels to categorical one-hot encoding
+    # label 5 -> [0,0,0,0,0,1,0,0,0,0]
+    y_train = to_categorical(y_train, num_classes=10)
+    y_test = to_categorical(y_test, num_classes=10)
 
     return x_train, y_train, x_test, y_test
 
 
 if __name__ == "__main__":
-    load_mnist()
+    x_train, y_train, x_test, y_test = load_mnist()
+    
+    print(y_train[0])
